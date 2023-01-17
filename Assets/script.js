0eeeea1117abf5832e5cfe0c7073e01b458eb337
Haley T <hntrimpert@yahoo.com>
2023-01-16 22:55:26 -0500

    some added javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var number = "1234567890"
 
 // Write password to the #password input
+function generatePassword() { 
+  window.prompt("How many characters will your password be?")
+  window.prompt("Do you need special characters?")
+  window.prompt("Do you need upper case letters?")
+  window.prompt("Do you need lower case letters?")
+  window.prompt("Do you need numbers?")
+}
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
